b1298ea19f6a9b5000b7c26bf2f0758de31e09b6
larryzodiac <evanmachale@gmail.com>
2020-01-23 16:14:39 +0000

    Stealing funky styles from Glitch.com

--- pictionary/sketch.js
+++ pictionary/sketch.js
@@ -1,22 +1,29 @@
 /*
-	PLaying with SketchRNN.
+	23.01.2020
+	Pictionary
 	https://github.com/ml5js/ml5-library/blob/master/src/SketchRNN/models.js
 	https://learn.ml5js.org/docs/#/reference/sketchrnn
 */
 
 // console.log('ml5 version:', ml5.version);
 
-let canvas;
+const id = 'canvas'
+let canvas, canvas_width, canvas_height;
+
 let classifier;
 let label;
 let confidence;
 
 function setup() {
-	canvas = createCanvas(windowWidth, windowHeight);
+	canvas_width = document.getElementById(id).offsetWidth;
+	canvas_height = document.getElementById(id).offsetHeight;
+  canvas = createCanvas(canvas_width, canvas_height);
+  canvas.parent(id);
 	// Initialize the Image Classifier method with MobileNet
 	classifier = ml5.imageClassifier('DoodleNet', () => console.log('Model Loaded!'));
 }
 
 function draw() {
-
+	fill(0)
+	ellipse(10,10,10,10);
 }
